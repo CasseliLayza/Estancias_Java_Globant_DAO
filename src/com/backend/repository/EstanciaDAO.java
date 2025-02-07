3e0926f42bdae974e18d96646b08bf28d7876fde
LuisAlgol <knellluis@gmail.com>
2025-02-06 21:02:06 -0300

    agregar clase EstanciaDao

--- src/com/backend/repository/EstanciaDAO.java
+++ src/com/backend/repository/EstanciaDAO.java
@@ -1,5 +1,47 @@
 package com.backend.repository;
 
-public class EstanciaDAO {
+import java.util.ArrayList;
+import java.util.List;
+
+import com.backend.entity.Estancia;
+
+public class EstanciaDAO extends DAO{
+
+    public void registrarEstancia(Estancia e) throws Exception{
+
+        String sql = "INSERT INTO estanciass (nombre_huesped, fecha_desde, fecha_hasta ) VALUES " +
+        "(" +
+        "'" + e.getNombreHuesped() + "'," +
+        "'" + e.getFechaDesde() + "'," +
+        "'" + e.getFechaHasta() + "'," +
+        ")";
+        insertarModificarEliminarDataBase(sql);
+     }
+    
+
+
+
+    
+    public List<Estancia> listarEstancia() throws Exception{
+     
+        String sql = "SELECT id_estancia, id_cliente, id_casa, nombre_huesped, fecha_desde, fecha hasta FROM estancias";
+        consultarDataBase(sql);
+
+        List<Estancia> estancias = new ArrayList<>();
+        while (resultSet.next()) {
+        Estancia estancia = new Estancia();
+        estancia.setIdEstancia(resultSet.getInt("id_estancia"));
+        estancia.setIdCliente(resultSet.getInt("id_cliente"));
+        estancia.setNombreHuesped(resultSet.getString("nombre_huespedl"));;
+        estancia.setFechaDesde(resultSet.getDate("fecha_desde"));
+        estancia.setFechaHasta(resultSet.getDate("fecha_hasta"));
+
+        estancias.add(estancia);
+        System.out.println(estancia.toString());
+        System.out.println(" ");
+        }
+        
+        return estancias;
+
 
 }
